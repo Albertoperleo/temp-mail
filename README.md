17930de47e7ed5ec3c511236e132b67d418323a7
albperleo <albperleo@alum.us.es>
2022-10-01 01:17:27 +0200

    Finished Fixes #2 #4

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A functional temporal mail address to avoid spam, trash-mails and moreover...
 
 ## Resources
 [Check the used api](https://www.1secmail.com/)
-##Libraries
+
+## Libraries
 - Requests 
 - Time
--- src/main.py
+++ src/main.py
@@ -4,16 +4,25 @@ import time
 api_url = 'https://www.1secmail.com/api/v1/'
 gen_mail = '?action=genRandomMailbox&count=1'
 
-new_mail = req.get(api_url+gen_mail).json()
-print(new_mail[0]) #returns an array
+genMail = req.request("GET", api_url + gen_mail).json()
+print('Operative mail: ' + genMail[0]) #returns an array
 
-#parsing the mail to build the mailbox req
-parse = new_mail.split('@')
-'''
-name = parse[0]
-dom = parse[1]
-print(name, dom)
-'''
+#parsing the mail to build the mailbox req or readMail req
+mail = genMail[0].split('@')
+getMailbox = '?action=getMessages&login='+mail[0]+'&domain='+mail[1]
+getMail = '?action=readMessage&login=' + mail[0] + '&domain=' + mail[1] + '&id='
 
+#check mail-box
+last_mail = None
+while(True):
+    time.sleep(5)
+    mailbox = req.request("GET", api_url + getMailbox).json()
+    if len(mailbox) > 0 and last_mail != mailbox[0]:
+        print(mailbox[0])
+        last_mail = mailbox[0]
+        getById = getMail + str(last_mail["id"])
+        res = req.request("GET", api_url + getById).json()
+        readMail = res["textBody"]
 
-#check_mailbox = '?action=getMessages&login='+ +'&domain='+
+        for line in readMail.split('\n'):
+            print(line)
